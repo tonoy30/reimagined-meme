3b8e04276618c286a41966670b7cd116ca1db57d
Tonoy Akanda <tonoy@monetizenow.io>
2023-10-29 23:07:37 +0600

    seed data

--- RecapApi/DbContexts/ApplicationDbContext.cs
+++ RecapApi/DbContexts/ApplicationDbContext.cs
@@ -1,12 +1,25 @@
 using Microsoft.EntityFrameworkCore;
+using RecapApi.Configurations;
 using RecapApi.Entities;
 
 namespace RecapApi.DbContexts;
 
 public class ApplicationDbContext : DbContext
 {
-    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    private readonly IWebHostEnvironment _environment;
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options, IWebHostEnvironment environment) :
+        base(options)
     {
+        _environment = environment;
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        if (_environment.IsDevelopment())
+        {
+            modelBuilder.ApplyConfiguration(new TodoConfiguration());
+        }
     }
 
     public DbSet<Todo>? Todos { get; set; }
